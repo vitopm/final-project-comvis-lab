d4fe74236d4a93cbcad537373872c77239261c09
Vito <vito.pramudita1502@gmail.com>
2022-12-21 12:44:31 +0700

    Finalized function

--- main.py
+++ main.py
@@ -14,21 +14,6 @@ def get_path_list(root_path):
     
     return train_names
 
-    '''
-        To get a list of path directories from root path
-
-        Parameters
-        ----------
-        root_path : str
-            Location of root directory
-        
-        Returns
-        -------
-        list
-            List containing the names of the sub-directories in the
-            root directory
-    '''
-
 def get_class_id(root_path, train_names):
     image_train_list = []
     image_class_id = []
@@ -42,27 +27,8 @@ def get_class_id(root_path, train_names):
 
             image_train_list.append (img_gray)
             image_class_id.append (index)
-    # print(zip(image_train_list,image_class_id))
-    # for a,b in zip(image_train_list,image_class_id):
-    #     print(f'{a} - {b}')
-    return image_train_list, image_class_id
             
-    '''
-        To get a list of train images and a list of image classes id
-
-        Parameters
-        ----------
-        root_path : str
-            Location of images root directory
-        train_names : list
-            List containing the names of the train sub-directories
-        Returns
-        -------
-        list
-            List containing all image in the train directories
-        list
-            List containing all image classes id
-    '''
+    return image_train_list, image_class_id
 
 def detect_faces_and_filter(image_list, image_classes_list=None):
 
@@ -70,202 +36,129 @@ def detect_faces_and_filter(image_list, image_classes_list=None):
 
     face_list = []
     class_list = []
-
-    for image, image_class in zip(image_list, image_classes_list):
-
-        detected_faces = face_cascade.detectMultiScale(
-            image,
-            scaleFactor = 1.2,
-            minNeighbors = 5
-        )
-
-        if(len(detected_faces) < 1):
-            continue
-
-        for face_rect in detected_faces:
-            x, y, w, h = face_rect
-            face_img = image[y:y+w, x:x+h]
-            face_list.append(face_img)
-            class_list.append(image_class)
-
-    return face_list, face_img, class_list
+    face_rects = []
     
-    '''
-        To detect a face from given image list and filter it if the face on
-        the given image is less than one
+    if image_classes_list is not None: #train
+        for image, image_class in zip(image_list, image_classes_list):
 
-        Parameters
-        ----------
-        image_list : list
-            List containing all loaded images
-        image_classes_list : list, optional
-            List containing all image classes id
-        
-        Returns
-        -------
-        list
-            List containing all filtered and cropped face images in grayscale
-        list
-            List containing all filtered faces location saved in rectangle (x, y, w, h)
-        list
-            List containing all filtered image classes id
-    ''' 
+            detected_faces = face_cascade.detectMultiScale(
+                image,
+                scaleFactor = 1.3,
+                minNeighbors = 3
+            )
+
+            if(len(detected_faces) < 1):
+                continue
+
+            for face_rect in detected_faces:
+                x, y, w, h = face_rect
+                face_img = image[y:y+w, x:x+h]
+                
+                face_list.append(face_img)
+                class_list.append(image_class)
+
+        return face_list, face_rects, class_list
+    else: # test
+        for image in image_list:
+
+            image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+            detected_faces = face_cascade.detectMultiScale(
+                image,
+                scaleFactor = 1.3,
+                minNeighbors = 3
+            )
+
+            if(len(detected_faces) < 1):
+                continue
+
+            for face_rect in detected_faces:
+                x, y, w, h = face_rect
+                face_img = image[y:y+w, x:x+h]
+                
+                face_list.append(face_img)
+                face_rects.append([x,y,w,h])
+
+        return face_list, face_rects, class_list
 
 def train(train_face_grays, image_classes_list):
-
 
     face_recognizer = cv.face.LBPHFaceRecognizer_create()
     face_recognizer.train(
         train_face_grays,
         np.array(image_classes_list)
     )
-    # print('yay')
     return face_recognizer
-    '''
-        To create and train face recognizer object
-
-        Parameters
-        ----------
-        train_face_grays : list
-            List containing all filtered and cropped face images in grayscale
-        image_classes_list : list
-            List containing all filtered image classes id
-        
-        Returns
-        -------
-        object
-            Recognizer object after being trained with cropped face images
-    '''
 
 def get_test_images_data(test_root_path):
     images_test = os.listdir (test_root_path)
     image_test_list = list()
     for image in (images_test):
         test_path = f'{test_root_path}/{image}'
-        image = cv.imread(test_path, 0)
+        image = cv.imread(test_path)
         image_test_list.append(image)
-        # print('halo')
-    # print(image_test_list)
-    return image_test_list
-    '''
-        To load a list of test images from given path list
 
-        Parameters
-        ----------
-        test_root_path : str
-            Location of images root directory
-        
-        Returns
-        -------
-        list
-            List containing all loaded gray test images
-    '''
+    return image_test_list
     
 def predict(recognizer, test_faces_gray):
+    results = list()
 
-
-
-    '''
-        To predict the test image with the recognizer
-
-        Parameters
-        ----------
-        recognizer : object
-            Recognizer object after being trained with cropped face images
-        train_face_grays : list
-            List containing all filtered and cropped face images in grayscale
-
-        Returns
-        -------
-        list
-            List containing all prediction results from given test faces
-    '''
+    for test_face in test_faces_gray:    
+        res, _ = recognizer.predict(test_face)
+        results.append(res)
+        
+    return results
     
 def draw_prediction_results(predict_results, test_image_list, test_faces_rects, train_names):
-    '''
-        To draw prediction results on the given test images and acceptance status
 
-        Parameters
-        ----------
-        predict_results : list
-            List containing all prediction results from given test faces
-        test_image_list : list
-            List containing all loaded test images
-        test_faces_rects : list
-            List containing all filtered faces location saved in rectangle
-        train_names : list
-            List containing the names of the train sub-directories
+    image_predict_results = list()
 
-        Returns
-        -------
-        list
-            List containing all test images after being drawn with
-            final result
-    '''
+    for predict_result, test_image, face_rect in zip(predict_results, test_image_list, test_faces_rects):
+        x, y, w, h = face_rect
+        face_img = test_image [y:y+w, x:x+h]
+        
+        test_img = cv.rectangle(
+            test_image,
+            (x,y), 
+            (x+w, y+h), 
+            (0, 255, 0), 
+            1
+        )
+        
+        if train_names[predict_result] in ['Pewdiepie', 'Jacksepticeye']:
+            text = f'{train_names[predict_result]} - Youtube'
+        else:
+            text = f'{train_names[predict_result]} - Twitch'
+
+        test_image = cv.putText (test_img, text, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        image_predict_results.append(test_image)
+
+    return image_predict_results
 
 def combine_and_show_result(image_list):
-    '''
-        To show the final image that already combine into one image
+    plt.figure(figsize=(10,7))
+    for index, image in enumerate(image_list):
+        plt.subplot(2, 3, index+1)
+        plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+        plt.xticks([])
+        plt.yticks([])
+    plt.tight_layout()
+    plt.show()
 
-        Parameters
-        ----------
-        image_list : nparray
-            Array containing image data
-    '''
-
-'''
-You may modify the code below if it's marked between
-
--------------------
-Modifiable
--------------------
-
-and
-
--------------------
-End of modifiable
--------------------
-'''
 if __name__ == "__main__":
 
-    '''
-        Please modify train_root_path value according to the location of
-        your data train root directory
-
-        -------------------
-        Modifiable
-        -------------------
-    '''
     train_root_path = "dataset/train"
-    '''
-        -------------------
-        End of modifiable
-        -------------------
-    '''
 
     train_names = get_path_list(train_root_path) #labels_list
     train_image_list, image_classes_list = get_class_id(train_root_path, train_names) #faces, indexes
     train_face_grays, _, filtered_classes_list = detect_faces_and_filter(train_image_list, image_classes_list)
     recognizer = train(train_face_grays, filtered_classes_list)
 
-    '''
-        Please modify train_root_path value according to the location of
-        your data train root directory
-
-        -------------------
-        Modifiable
-        -------------------
-    '''
     test_root_path = "dataset/test"
-    '''
-        -------------------
-        End of modifiable
-        -------------------
-    '''
 
     test_image_list = get_test_images_data(test_root_path)
     test_faces_gray, test_faces_rects, _ = detect_faces_and_filter(test_image_list)
     predict_results = predict(recognizer, test_faces_gray)
-    # predicted_test_image_list = draw_prediction_results(predict_results, test_image_list, test_faces_rects, train_names)
+    predicted_test_image_list = draw_prediction_results(predict_results, test_image_list, test_faces_rects, train_names)
     
-    # combine_and_show_result(predicted_test_image_list)
+    combine_and_show_result(predicted_test_image_list)
